840c199c21a513d2a33dfb3c6aba33736f11c6ae
galexand <alexandre.giordana.aragon2@gmail.com>
2024-03-22 16:44:21 +0100

    wip season changes

--- api/src/Repository/SeasonRepository.php
+++ api/src/Repository/SeasonRepository.php
@@ -2,7 +2,7 @@
 
 namespace App\Repository;
 
-use App\Entity\Seasons;
+use App\Entity\Season;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
@@ -18,7 +18,7 @@ class SeasonsRepository extends ServiceEntityRepository
 {
     public function __construct(ManagerRegistry $registry)
     {
-        parent::__construct($registry, Seasons::class);
+        parent::__construct($registry, Season::class);
     }
 
 //    /**
