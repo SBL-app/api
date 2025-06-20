653332e7f882b15701775b7783d0b3068791662d
galexand <71892736+nnaova@users.noreply.github.com>
2025-06-20 16:18:05 +0200

    disable post, put, patch & delete route

--- src/Controller/SeasonController.php
+++ src/Controller/SeasonController.php
@@ -150,66 +150,66 @@ class SeasonController extends AbstractController
         ]);
     }
 
-    #[Route('/season', name: 'app_season_create', methods: ['POST'])]
-    public function createSeason(Request $request, Season $season, EntityManager $em): JsonResponse
-    {
-        $data = json_decode($request->getContent(), true);
-        $season->setName($data['name']);
-        $season->setStartDate(new \DateTime($data['start_date']));
-        $season->setEndDate(new \DateTime($data['end_date']));
-        $em->persist($season);
-        $em->flush();
-        return $this->json([
-            'id' => $season->getId(),
-            'name' => $season->getName(),
-            'start_date' => $season->getStartDate()->format('d-m-Y'),
-            'end_date' => $season->getEndDate()->format('d-m-Y')
-        ]);
-    }
+    // #[Route('/season', name: 'app_season_create', methods: ['POST'])]
+    // public function createSeason(Request $request, Season $season, EntityManager $em): JsonResponse
+    // {
+    //     $data = json_decode($request->getContent(), true);
+    //     $season->setName($data['name']);
+    //     $season->setStartDate(new \DateTime($data['start_date']));
+    //     $season->setEndDate(new \DateTime($data['end_date']));
+    //     $em->persist($season);
+    //     $em->flush();
+    //     return $this->json([
+    //         'id' => $season->getId(),
+    //         'name' => $season->getName(),
+    //         'start_date' => $season->getStartDate()->format('d-m-Y'),
+    //         'end_date' => $season->getEndDate()->format('d-m-Y')
+    //     ]);
+    // }
 
-    #[Route('/season/{id}', name: 'app_season_update', methods: ['PUT'])]
-    public function updateSeason(Request $request, Season $season, EntityManager $em): JsonResponse
-    {
-        $data = json_decode($request->getContent(), true);
-        $season->setName($data['name']);
-        $season->setStartDate(new \DateTime($data['start_date']));
-        $season->setEndDate(new \DateTime($data['end_date']));
-        $em->flush();
-        return $this->json([
-            'id' => $season->getId(),
-            'name' => $season->getName(),
-            'start_date' => $season->getStartDate()->format('d-m-Y'),
-            'end_date' => $season->getEndDate()->format('d-m-Y')
-        ]);
-    }
+    // #[Route('/season/{id}', name: 'app_season_update', methods: ['PUT'])]
+    // public function updateSeason(Request $request, Season $season, EntityManager $em): JsonResponse
+    // {
+    //     $data = json_decode($request->getContent(), true);
+    //     $season->setName($data['name']);
+    //     $season->setStartDate(new \DateTime($data['start_date']));
+    //     $season->setEndDate(new \DateTime($data['end_date']));
+    //     $em->flush();
+    //     return $this->json([
+    //         'id' => $season->getId(),
+    //         'name' => $season->getName(),
+    //         'start_date' => $season->getStartDate()->format('d-m-Y'),
+    //         'end_date' => $season->getEndDate()->format('d-m-Y')
+    //     ]);
+    // }
 
-    #[Route('/season/{id}', name: 'app_season_patch', methods: ['PATCH'])]
-    public function patchSeason(Request $request, Season $season, EntityManager $em): JsonResponse
-    {
-        $data = json_decode($request->getContent(), true);
-        if (isset($data['name'])) {
-            $season->setName($data['name']);
-        }
-        if (isset($data['start_date'])) {
-            $season->setStartDate(new \DateTime($data['start_date']));
-        }
-        if (isset($data['end_date'])) {
-            $season->setEndDate(new \DateTime($data['end_date']));
-        }
-        $em->flush();
-        return $this->json([
-            'id' => $season->getId(),
-            'name' => $season->getName(),
-            'start_date' => $season->getStartDate()->format('d-m-Y'),
-            'end_date' => $season->getEndDate()->format('d-m-Y')
-        ]);
-    }
+    // #[Route('/season/{id}', name: 'app_season_patch', methods: ['PATCH'])]
+    // public function patchSeason(Request $request, Season $season, EntityManager $em): JsonResponse
+    // {
+    //     $data = json_decode($request->getContent(), true);
+    //     if (isset($data['name'])) {
+    //         $season->setName($data['name']);
+    //     }
+    //     if (isset($data['start_date'])) {
+    //         $season->setStartDate(new \DateTime($data['start_date']));
+    //     }
+    //     if (isset($data['end_date'])) {
+    //         $season->setEndDate(new \DateTime($data['end_date']));
+    //     }
+    //     $em->flush();
+    //     return $this->json([
+    //         'id' => $season->getId(),
+    //         'name' => $season->getName(),
+    //         'start_date' => $season->getStartDate()->format('d-m-Y'),
+    //         'end_date' => $season->getEndDate()->format('d-m-Y')
+    //     ]);
+    // }
 
-    #[Route('/season/{id}', name: 'app_season_delete', methods: ['DELETE'])]
-    public function deleteSeason(Season $season, EntityManager $em): JsonResponse
-    {
-        $em->remove($season);
-        $em->flush();
-        return new JsonResponse(['message' => 'Season deleted successfully'], 200);
-    }
+    // #[Route('/season/{id}', name: 'app_season_delete', methods: ['DELETE'])]
+    // public function deleteSeason(Season $season, EntityManager $em): JsonResponse
+    // {
+    //     $em->remove($season);
+    //     $em->flush();
+    //     return new JsonResponse(['message' => 'Season deleted successfully'], 200);
+    // }
 }
