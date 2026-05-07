86cf4323b018f97edd0e953aa6acefc5f2d358b7
galexand <giordana.alex@gmail.com>
2026-05-07 14:59:14 +0200

    feat: add GameResult entity and repository

---
+++ src/Repository/GameResultRepository.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Game;
+use App\Entity\GameResult;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<GameResult>
+ */
+class GameResultRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, GameResult::class);
+    }
+
+    public function findPendingByGame(Game $game): ?GameResult
+    {
+        return $this->findOneBy([
+            'game' => $game,
+            'status' => GameResult::STATUS_PENDING_VALIDATION,
+        ]);
+    }
+
+    public function findDisputedByGame(Game $game): ?GameResult
+    {
+        return $this->findOneBy([
+            'game' => $game,
+            'status' => GameResult::STATUS_DISPUTED,
+        ]);
+    }
+
+    public function findLatestByGame(Game $game): ?GameResult
+    {
+        return $this->createQueryBuilder('gr')
+            ->where('gr.game = :game')
+            ->setParameter('game', $game)
+            ->orderBy('gr.createdAt', 'DESC')
+            ->setMaxResults(1)
+            ->getQuery()
+            ->getOneOrNullResult();
+    }
+}
